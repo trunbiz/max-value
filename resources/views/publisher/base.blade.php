972b38839575cf3eb74dce4dd66f2c251f5515f4
trunbiz <trunbiz.dev@gmail.com>
2023-10-16 17:17:33 +0700

    update

--- resources/views/publisher/base.blade.php
+++ resources/views/publisher/base.blade.php
@@ -160,52 +160,52 @@ $skype = !empty($userAssign) ? \App\Models\User::find($userAssign->user_id)->sky
     </div><!-- dropdown -->
 
     <div class="dropdown dropdown-notification ms-3 ms-xl-4">
-        <a href="" class="dropdown-link" data-bs-toggle="dropdown" data-bs-auto-close="outside"><small>3</small><i
+        <a href="" class="dropdown-link" data-bs-toggle="dropdown" data-bs-auto-close="outside"><i
                 class="ri-notification-3-line"></i></a>
         <div class="dropdown-menu dropdown-menu-end mt-10-f me--10-f">
             <div class="dropdown-menu-header">
                 <h6 class="dropdown-menu-title">Notifications</h6>
             </div><!-- dropdown-menu-header -->
-            <ul class="list-group">
-                <li class="list-group-item">
-                    <div class="avatar online"><img src="assets/img/img10.jpg" alt=""></div>
-                    <div class="list-group-body">
-                        <p><strong>Dominador Manuel</strong> and <strong>100 other people</strong> reacted to your
-                            comment "Tell your partner that...</p>
-                        <span>Aug 20 08:55am</span>
-                    </div><!-- list-group-body -->
-                </li>
-                <li class="list-group-item">
-                    <div class="avatar online"><img src="assets/img/img11.jpg" alt=""></div>
-                    <div class="list-group-body">
-                        <p><strong>Angela Ighot</strong> tagged you and <strong>9 others</strong> in a post.</p>
-                        <span>Aug 18 10:30am</span>
-                    </div><!-- list-group-body -->
-                </li>
-                <li class="list-group-item">
-                    <div class="avatar"><span class="avatar-initial bg-primary">a</span></div>
-                    <div class="list-group-body">
-                        <p>New listings were added that match your search alert <strong>house for rent</strong></p>
-                        <span>Aug 15 08:10pm</span>
-                    </div><!-- list-group-body -->
-                </li>
-                <li class="list-group-item">
-                    <div class="avatar online"><img src="assets/img/img14.jpg" alt=""></div>
-                    <div class="list-group-body">
-                        <p>Reminder: <strong>Jerry Cuares</strong> invited you to like <strong>Cuares Surveying
-                                Services</strong>. <br><a href="">Accept</a> or <a href="">Decline</a></p>
-                        <span>Aug 14 11:50pm</span>
-                    </div><!-- list-group-body -->
-                </li>
-                <li class="list-group-item">
-                    <div class="avatar online"><img src="assets/img/img15.jpg" alt=""></div>
-                    <div class="list-group-body">
-                        <p><strong>Dyanne Aceron</strong> reacted to your post <strong>King of the Bed</strong>.</p>
-                        <span>Aug 10 05:30am</span>
-                    </div><!-- list-group-body -->
-                </li>
-            </ul>
-            <div class="dropdown-menu-footer"><a href="">Show all Notifications</a></div>
+{{--            <ul class="list-group">--}}
+{{--                <li class="list-group-item">--}}
+{{--                    <div class="avatar online"><img src="assets/img/img10.jpg" alt=""></div>--}}
+{{--                    <div class="list-group-body">--}}
+{{--                        <p><strong>Dominador Manuel</strong> and <strong>100 other people</strong> reacted to your--}}
+{{--                            comment "Tell your partner that...</p>--}}
+{{--                        <span>Aug 20 08:55am</span>--}}
+{{--                    </div><!-- list-group-body -->--}}
+{{--                </li>--}}
+{{--                <li class="list-group-item">--}}
+{{--                    <div class="avatar online"><img src="assets/img/img11.jpg" alt=""></div>--}}
+{{--                    <div class="list-group-body">--}}
+{{--                        <p><strong>Angela Ighot</strong> tagged you and <strong>9 others</strong> in a post.</p>--}}
+{{--                        <span>Aug 18 10:30am</span>--}}
+{{--                    </div><!-- list-group-body -->--}}
+{{--                </li>--}}
+{{--                <li class="list-group-item">--}}
+{{--                    <div class="avatar"><span class="avatar-initial bg-primary">a</span></div>--}}
+{{--                    <div class="list-group-body">--}}
+{{--                        <p>New listings were added that match your search alert <strong>house for rent</strong></p>--}}
+{{--                        <span>Aug 15 08:10pm</span>--}}
+{{--                    </div><!-- list-group-body -->--}}
+{{--                </li>--}}
+{{--                <li class="list-group-item">--}}
+{{--                    <div class="avatar online"><img src="assets/img/img14.jpg" alt=""></div>--}}
+{{--                    <div class="list-group-body">--}}
+{{--                        <p>Reminder: <strong>Jerry Cuares</strong> invited you to like <strong>Cuares Surveying--}}
+{{--                                Services</strong>. <br><a href="">Accept</a> or <a href="">Decline</a></p>--}}
+{{--                        <span>Aug 14 11:50pm</span>--}}
+{{--                    </div><!-- list-group-body -->--}}
+{{--                </li>--}}
+{{--                <li class="list-group-item">--}}
+{{--                    <div class="avatar online"><img src="assets/img/img15.jpg" alt=""></div>--}}
+{{--                    <div class="list-group-body">--}}
+{{--                        <p><strong>Dyanne Aceron</strong> reacted to your post <strong>King of the Bed</strong>.</p>--}}
+{{--                        <span>Aug 10 05:30am</span>--}}
+{{--                    </div><!-- list-group-body -->--}}
+{{--                </li>--}}
+{{--            </ul>--}}
+{{--            <div class="dropdown-menu-footer"><a href="">Show all Notifications</a></div>--}}
         </div><!-- dropdown-menu -->
     </div><!-- dropdown -->
     <div class="dropdown dropdown-profile ms-3 ms-xl-4">
