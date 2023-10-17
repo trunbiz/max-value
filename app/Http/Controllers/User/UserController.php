ac0d1e979de0c809234209e8783948d5d5e69e69
trunbiz <trunbiz.dev@gmail.com>
2023-10-17 16:09:17 +0700

    sai pass login

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -31,7 +31,6 @@ class UserController extends Controller
 
     public function postLoginUser(Request $request)
     {
-        dd(2332);
         $remember = $request->has('remember_me') ? true : false;
         if (auth()->attempt([
             'email' => $request->email,
