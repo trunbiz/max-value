efda912362136f08e72355652c3304280c5e919e
trunbiz <trunbiz.dev@gmail.com>
2023-11-17 09:22:50 +0700

    seeder ads

---
+++ database/seeders/AddAdsInSettingSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+
+class AddAdsInSettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $adsTxt = File::get(public_path('../../public_html/ads.txt'));
+
+        $settingInfo = Setting::find(1);
+        $settingInfo->ads_txt = $adsTxt;
+        $settingInfo->save();
+    }
+}
